2297b97cece1d2f2beec12de60b490320b0b5504
Demysel <84205147+Demysel@users.noreply.github.com>
2025-01-24 13:53:22 +0100

    Update authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,1 +1,23 @@
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+exports.register = async (req, res) => {
+  const { username, password } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const newUser = new User({ username, password: hashedPassword });
+  await newUser.save();
+  res.status(201).send('User registered');
+};
+
+exports.login = async (req, res) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username });
+  if (user && await bcrypt.compare(password, user.password)) {
+    const token = jwt.sign({ userId: user._id }, 'your_jwt_secret');
+    res.status(200).json({ token });
+  } else {
+    res.status(401).send('Invalid credentials');
+  }
+};
 
