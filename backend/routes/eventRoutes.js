7f3d8a635fb090260783a24a5ec61e093ff16217
Demysel <84205147+Demysel@users.noreply.github.com>
2025-01-24 13:52:58 +0100

    Update eventRoutes.js

--- backend/routes/eventRoutes.js
+++ backend/routes/eventRoutes.js
@@ -1,1 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const eventController = require('../controllers/eventController');
+
+router.post('/', eventController.createEvent);
+router.get('/', eventController.getEvents);
+
+module.exports = router;
 
