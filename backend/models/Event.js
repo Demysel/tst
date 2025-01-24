ffb7da42422983f8457407d0b998353bc921a265
Demysel <84205147+Demysel@users.noreply.github.com>
2025-01-24 14:20:31 +0100

    Update Event.js

--- backend/models/Event.js
+++ backend/models/Event.js
@@ -1,1 +1,10 @@
-j
+const mongoose = require('mongoose');
+
+const eventSchema = new mongoose.Schema({
+  title: String,
+  date: Date,
+  type: String,
+  userId: mongoose.Schema.Types.ObjectId,
+});
+
+module.exports = mongoose.model('Event', eventSchema);
