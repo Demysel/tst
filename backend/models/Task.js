3581e1d463193a1b31ccc996a3c424b417e4ba62
Demysel <84205147+Demysel@users.noreply.github.com>
2025-01-24 14:19:58 +0100

    Update Task.js

--- backend/models/Task.js
+++ backend/models/Task.js
@@ -1,1 +1,10 @@
+const mongoose = require('mongoose');
+
+const taskSchema = new mongoose.Schema({
+  item: String,
+  quantity: Number,
+  userId: mongoose.Schema.Types.ObjectId,
+});
+
+module.exports = mongoose.model('Task', taskSchema);
  
