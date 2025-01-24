e7fa4fbd15545840a7aea945228ce8e5b926b663
Demysel <84205147+Demysel@users.noreply.github.com>
2025-01-24 13:52:38 +0100

    Update eventController.js

--- backend/controllers/eventController.js
+++ backend/controllers/eventController.js
@@ -1,1 +1,15 @@
+const Event = require('../models/Event');
+
+exports.createEvent = async (req, res) => {
+  const { title, date, type, userId } = req.body;
+  const newEvent = new Event({ title, date, type, userId });
+  await newEvent.save();
+  res.status(201).send('Event created');
+};
+
+exports.getEvents = async (req, res) => {
+  const { userId } = req.query;
+  const events = await Event.find({ userId });
+  res.status(200).json(events);
+};
 
