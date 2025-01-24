73f8af755e6d8552313c00bb6b38d4596c2c37a1
Demysel <84205147+Demysel@users.noreply.github.com>
2025-01-24 14:19:23 +0100

    Update Note.js

--- backend/models/Note.js
+++ backend/models/Note.js
@@ -1,1 +1,9 @@
+const mongoose = require('mongoose');
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  userId: mongoose.Schema.Types.ObjectId,
+});
+
+module.exports = mongoose.model('Note', noteSchema);
  
