cbc4336bab279d5dc2d2c8f75ede1654c8779ed2
Demysel <84205147+Demysel@users.noreply.github.com>
2025-01-24 13:49:53 +0100

    Update noteController.js

--- backend/controllers/noteController.js
+++ backend/controllers/noteController.js
@@ -1,1 +1,15 @@
+const Note = require('../models/Note');
+
+exports.createNote = async (req, res) => {
+  const { content, userId } = req.body;
+  const newNote = new Note({ content, userId });
+  await newNote.save();
+  res.status(201).send('Note created');
+};
+
+exports.getNotes = async (req, res) => {
+  const { userId } = req.query;
+  const notes = await Note.find({ userId });
+  res.status(200).json(notes);
+};
 
