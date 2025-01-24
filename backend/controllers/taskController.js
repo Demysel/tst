833c442beb72fb13ce53e945ba1ee618b2113c30
Demysel <84205147+Demysel@users.noreply.github.com>
2025-01-24 13:51:37 +0100

    Update taskController.js

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -1,1 +1,15 @@
+const Task = require('../models/Task');
+
+exports.createTask = async (req, res) => {
+  const { item, quantity, userId } = req.body;
+  const newTask = new Task({ item, quantity, userId });
+  await newTask.save();
+  res.status(201).send('Task created');
+};
+
+exports.getTasks = async (req, res) => {
+  const { userId } = req.query;
+  const tasks = await Task.find({ userId });
+  res.status(200).json(tasks);
+};
 
